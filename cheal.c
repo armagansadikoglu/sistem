47822bfcfb9052fdfef0a4c4e4158fab1ed7727e
Tolgahan <tolgahan.cepel@gmail.com>
2020-04-11 16:16:16 +0300

    yorum satirlari

--- cheal.c
+++ cheal.c
@@ -7,32 +7,6 @@
 #include "./lib/jval.h"
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /*This struct will be used to hold global information*/
 typedef struct information
 {
@@ -47,9 +21,7 @@ typedef struct information
 }info;
 
 
-
-
-/*this will perform a Depth first search on all nodes that are children of the current player*/
+/* işlemdeki oyuncunun tüm çocukları arasında DFS araması yapılacak */
 void DFS( player * pp, int hop, info * Ip, int ttl_heal, player * from)
 {
 	int i, j, rslt, dfs_heal, old_pp, cnt;
@@ -164,6 +136,8 @@ void DFS( player * pp, int hop, info * Ip, int ttl_heal, player * from)
 
 int main( int argc, char** argv)
 {
+	/* argüman sayısı kontrol ediliyor */
+	if (argc != 6) { fprintf(stderr, "usage: printwords filename\n"); exit(1); }
 
 	char hldr[100];
 
@@ -173,18 +147,19 @@ int main( int argc, char** argv)
 
     player * pp;				
 
-	player * prv = NULL;				/*will store a pointer to the previous player*/
+	player * prv = NULL;				/* geçici olarak kullanılacak*/
 
-	player * UP = NULL;					/*Will be used to point to the player Urgosa*/
+	player * UP = NULL;					/* Lokman Hekimi gösteren player işaretçisi */
 
 	player_cnt = 0;
-	//process command line arguments and store in global information struct
 	
-	info *Ip = malloc(sizeof(info) );
+	// process command line arguments and store in global information struct
 	
-	Ip->c_j = atoi(argv[3]);
-	Ip->c_pp = atoi(argv[4]);
-	sscanf(argv[5],"%lf",&Ip->c_p_r);
+	info *Ip = malloc(sizeof(info));	/* bilgiler için yer ayırılıyor */
+
+	Ip->c_j = atoi(argv[3]);	/* num_jumps atanıyor */
+	Ip->c_pp = atoi(argv[4]);	/* initial_power atanıyor */
+	sscanf(argv[5],"%lf",&Ip->c_p_r);	/* power_reduction atanıyor */
 	Ip->amnt_heal = 0;
 	Ip->best_heal = 0;
 
@@ -194,77 +169,65 @@ int main( int argc, char** argv)
 	Ip->healing = (int *) malloc( sizeof( int * ) * atoi(argv[3]) );
 	
 
+	/* TEXT DOSYASINDAKİ SATIRLAR DÖNGÜSEL OLARAK OKUNUYOR */
 
-
-
-
-	//Now take in lines from std input and create nodes for each line/player
-	
-	while( scanf("%d %d %d %d %s", &x, &y, &c_pp, &mx_pp, hldr) > 0 )
+	while(scanf("%d %d %d %d %s", &x, &y, &c_pp, &mx_pp, hldr) > 0 )
 	{
+		pp = (player *)malloc(sizeof(player));	// oyuncu için yer ayırılıyor
+
+		pp->x = x;	// x değeri atanıyor
+		pp->y = y;	// y değeri atanıyor
+		pp->cur_PP = c_pp;	// mevcut gücü atanıyor
+		pp->max_PP = mx_pp;	// maksimum gücü atanıyor
+		pp->visited = -1;	// ziyaret edildi mi(-1 ise edilmedi)
+		pp->healing = 0;	// iyileştirme başlangıçta 0 atanıyor
+		pp->adj_size = 0;	// bitişik boyutu 0 atanıyor
+		strcpy(pp->name, hldr);	// adı kopyalanıyor
+		pp->prev = prv;	// önceki düğüm (ilk düğümde NULL)
+
+		/* lokman hekim okunuyorsa kayıt ediliyor */
+		if( strcmp(hldr, "Lokman_Hekim") == 0) UP = pp; 
 		
-		pp = (player *)malloc( sizeof(player) );
+		prv = pp;	// önceki oyuncu olarak atanıyor
 
-		pp->x = x;
-		(*pp).y = y;
-		(*pp).cur_PP = c_pp;
-		(*pp).max_PP = mx_pp;
-		(*pp).visited = -1;
-		(*pp).healing = 0;
-		(*pp).adj_size = 0;
-		strcpy( (*pp).name, hldr);
-		(*pp).prev = prv;
-		
-		/*check for Urgosa
-		  if found save a pointer to it
-		  Then set the current player as the previous player for linking purposes*/
-
-		if( strcmp(hldr, "Urgosa_the_Healing_Shaman") == 0) UP = pp; 
-		
-		prv = pp;
-
-		player_cnt++;
-
+		player_cnt++;	// oyuncu sayısı artılıyor
 	}
 
 
-
-	player *nodes[player_cnt];				/*will hold all of the nodes in the graph*/
+	player *nodes[player_cnt];				/*tüm oyuncu düğümleri tutulacak*/
 	player *edges[player_cnt];				/*will be used as a temporary player pointer contianer*/
 
-	//now make the graph by going through the nodes using there previous fields
-	//adding them to the graph array
+	// tüm düğümler taranıyor ve adresleri nodes içerisine atanıyor
 	for( i=0 ; i < player_cnt ; i++)
 	{
-	
 		nodes[i] = prv;
-		prv = (*prv).prev;
+		prv = prv->prev;
 	}
 
-
-
-	//go through all nodes and set the adjacent lists based on jump range
+	// tüm düğümler taranıyor ve bitişik olanları atanıyor
 	for( i = 0; i < player_cnt; i++)
 	{
-
-		//check the needed size of adjacent array for current node
-		//by going through and counting how many are possible based on the distance from current node
+		// check the needed size of adjacent array for current node
+		// by going through and counting how many are possible based on the distance from current node
+		
 		for(j = 0; j < player_cnt; j++)
 		{
-			//make sure not to count the node it self
+			// düğümün kendisini de saymaması gerekiyor
 			if(j != i)
 			{
-				x = pow( (double) ( (*nodes[i]).x - (*nodes[j]).x) ,2);
+				// uzaklık hesaplanıyor
+				// pow(üzerini almada kullanılıyor)
+				x = pow((double) ( (*nodes[i]).x - (*nodes[j]).x) ,2);
 				y = pow( (double) ( (*nodes[i]).y - (*nodes[j]).y) ,2);
 				dist = ( sqrt( x + y) );
 
-
 				/*check for those players that are within jump range if so add to edges temporarily and increment cnt*/
 				
+				// eğer jump_range değerinden kısaysa veya kenarlara atanıyor
 				if(dist <= atoi(argv[2])) 
 				{
 					edges[ nodes[i]->adj_size] = nodes[j];	
-
+					
 					nodes[i]->adj_size++;
 				}
 			}
@@ -310,7 +273,7 @@ int main( int argc, char** argv)
 		printf("%s %d\n", (Ip->best_path[i])->name, Ip->healing[i] );
 	}
 
-	printf("Total_Healing %d\n", Ip->best_heal); 
+	printf("Toplam Iyilesme %d\n", Ip->best_heal); 
 	
 	return 0;
 }
